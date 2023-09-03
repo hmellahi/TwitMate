320be053591b7972cddd76f0970a1cf31523b056
hmellahi <hamzasenpai307@gmail.com>
2023-09-03 18:05:57 +0100

    update public routes

--- app/api/clerk/webhooks/route.ts
+++ app/api/clerk/webhooks/route.ts
@@ -35,6 +35,7 @@ type Event = {
 };
 
 export const POST = async (request: Request) => {
+  console.log('was here')
   const payload = await request.json();
   const header = headers();
 
--- middleware.ts
+++ middleware.ts
@@ -37,7 +37,7 @@ async function ms(auth: AuthObject) {
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  // afterAuth: ms,
+  publicRoutes: ["/api/clerk/webhooks"]
 });
 
 export const config = {
