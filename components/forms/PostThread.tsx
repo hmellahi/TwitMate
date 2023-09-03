fd18142fb424d56e7ef0618a6a971c5ccb03a8cd
hmellahi <hamzasenpai307@gmail.com>
2023-09-03 17:14:39 +0100

    fix textarea

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Textarea } from "@/components/ui/Textarea";
+import { Textarea } from "@/components/ui/textarea";
 import {
   Form,
   FormControl,
