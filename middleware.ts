65bd777d6c0d2dd04ecf7988ca8eac7906d0dba2
hmellahi <hamzasenpai307@gmail.com>
2023-09-16 16:53:51 +0000

    enable debugging

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/clerk/webhooks"],
+  debug:true
 });
 
 export const config = {
