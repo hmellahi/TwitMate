b165bddd61d39b54c659721d9f047dccefeee1d5
hmellahi <hamzasenpai307@gmail.com>
2023-10-21 20:31:22 +0100

    cleaing

--- app/src/components/ui/MediaViewer.tsx
+++ app/src/components/ui/MediaViewer.tsx
@@ -24,7 +24,8 @@ export function MediaViewer({
             className="!relative w-full object-cover rounded-xl !h-auto bodder-2 border-white"
             alt={`Avatar ${index + 1}`}
             onLoad={onLoad}
-            sizes="(min-width: 820px) 506px, (min-width: 780px) calc(-305vw + 2915px), (min-width: 640px) calc(78.33vw - 59px), (min-width: 520px) 78vw, (min-width: 460px) calc(47.5vw + 112px), (min-width: 420px) 291px, (min-width: 360px) calc(47.5vw + 63px), calc(15vw + 145px)"
+            quality={75}
+            sizes="(min-width: 820px) 536px, (min-width: 780px) calc(-305vw + 2915px), (min-width: 640px) calc(78.33vw - 59px), (min-width: 520px) 78vw, (min-width: 460px) calc(47.5vw + 112px), (min-width: 420px) 291px, (min-width: 360px) calc(47.5vw + 63px), 536px"
             priority={isPriority}
           />
         </div>
@@ -32,36 +33,3 @@ export function MediaViewer({
     </div>
   );
 }
-
-// export function MediaViewer({
-//   imageURLs,
-//   className,
-//   onLoad,
-//   ...props
-// }: {
-//   imageURLs: Array<string>;
-//   className: string;
-// }) {
-//   if (imageURLs.length === 0) {
-//     return null; // Return null when there are no images to display
-//   }
-
-//   return (
-//     <div className={`w-full relative flex ${className}`} {...props}>
-//       {imageURLs.map((imageSrc, index) => (
-//         <div
-//           key={index}
-//           className="w-full relative border-2 border-white rounded-xl overflow-hidden"
-//         >
-//           <div
-//             className="absolute top-0 left-0 w-full"
-//             style={{
-//               paddingBottom: '100%', // Maintain a 1:1 aspect ratio
-//               background: `url(${imageSrc}) center/cover no-repeat`,
-//             }}
-//           ></div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
